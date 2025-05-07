3af39bd2f4e095eab3104cd584ad3afd50ddc0d8
Mohammed Anwar Shah <96904756+Mohammedanwarsha1@users.noreply.github.com>
2025-05-07 14:25:13 +0530

    Time: 0 ms (100%), Space: 14.9 MB (84.37%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -28,3 +28,24 @@ public:
         return ans;
     }
 };
+
+//using preoder
+// class Solution {
+// public:
+//     int level(TreeNode* root)
+//     {
+//         if(root==NULL) return 0;
+//         return 1 + max(level(root->left),level(root->right));
+//     }
+//     void preorder(TreeNode*root,vector<int> &ans,int level){
+//         if(root==NULL) return;
+//         ans[level]=root->val;
+//         preorder(root->left,ans,level+1);
+//         preorder(root->right,ans,level+1);
+//     }
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int>ans(level(root),0);
+//         preorder(root,ans,0);
+//         return ans;
+//     }
+// };
