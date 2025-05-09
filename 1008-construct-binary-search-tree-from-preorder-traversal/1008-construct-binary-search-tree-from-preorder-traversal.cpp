9e4f26416feed36eeac694baf326487d0f30c19c
Mohammed Anwar Shah <96904756+Mohammedanwarsha1@users.noreply.github.com>
2025-05-09 09:35:00 +0530

    Time: 0 ms (100%), Space: 15.7 MB (97.06%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL) root=new TreeNode(val);
+        else if(root->val > val){
+            if(root->left == NULL) root->left=new TreeNode(val);
+            else insertIntoBST(root->left,val);
+        }
+        else{
+            if(root->right==NULL) root->right=new TreeNode(val);
+            else insertIntoBST(root->right,val);
+        }
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+        TreeNode* root=new TreeNode(pre[0]);
+        for(int i=1;i<pre.size();i++){
+            insertIntoBST(root,pre[i]);
+        }
+        return root;
+    }
+};
