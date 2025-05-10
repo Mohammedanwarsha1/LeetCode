193637f665ff665ef0095cd3fe06fe4a63d3b06d
Mohammed Anwar Shah <96904756+Mohammedanwarsha1@users.noreply.github.com>
2025-05-10 11:15:07 +0530

    Time: 0 ms (100%), Space: 10.9 MB (34.39%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,15 +1,50 @@
 class Solution {
 public:
-    void inorder(TreeNode *root,vector<int> &ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
-
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root,ans);
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left != NULL) {
+                TreeNode* pred = curr->left;
+                while (pred->right != NULL && pred->right != curr) {
+                    pred = pred->right;
+                }
+                if (pred->right == NULL) {
+                    pred->right = curr;
+                    curr = curr->left;
+                } else {
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+
+            } else {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+        }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         stack<TreeNode*>st;
+//         vector<int> ans;
+//         TreeNode * node=root;
+//         while(st.size()>0 || node!=NULL){
+//             if(node){
+//                 st.push(node);
+//                 node=node->left;
+//             }
+//             else{
+//                 TreeNode * temp=st.top();
+//                 st.pop();
+//                 ans.push_back(temp->val);
+//                 node=temp->right;
+//             }
+//         }
+//         return ans;
+//     }
+// };
