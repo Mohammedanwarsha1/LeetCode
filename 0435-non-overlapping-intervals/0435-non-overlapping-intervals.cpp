d4b2b827bb4ad021a931a019565227f01f34791e
Mohammed Anwar Shah <96904756+Mohammedanwarsha1@users.noreply.github.com>
2025-06-02 17:12:15 +0530

    Time: 53 ms (46.42%), Space: 94 MB (67.25%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,3 +1,4 @@
+//sort based on end time
 bool cmp(vector<int> &a,vector<int> &b){
     return a[1] < b[1];
 }
@@ -18,3 +19,26 @@ public:
         return ans;
     }
 };
+
+//sort based on start time
+// bool cmp(vector<int> &a,vector<int> &b){
+//     return a[0] < b[0];
+// }
+// class Solution {
+// public:
+//     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(),intervals.end(),cmp);
+//         int ans=0;
+//         int lastEndTime=intervals[0][1];
+//         for(int i=1;i<intervals.size();i++){
+//             if(intervals[i][0]<lastEndTime){
+//                 ans++;
+//                 lastEndTime=min(lastEndTime,intervals[i][1]);
+//             }
+//             else{
+//                 lastEndTime=intervals[i][1];
+//             }
+//         }
+//         return ans;
+//     }
+// };
