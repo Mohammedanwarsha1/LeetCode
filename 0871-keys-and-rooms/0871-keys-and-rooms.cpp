4aeb8c403238d05a4642d0bb8a503da09f006838
Mohammed Anwar Shah <96904756+Mohammedanwarsha1@users.noreply.github.com>
2025-06-18 16:20:47 +0530

    Time: 4 ms (11.9%), Space: 15 MB (19.11%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> visited;
+        queue<int> q;
+        q.push(0);
+        visited.insert(0);
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            for(int neighbor:rooms[curr]){
+                if(visited.count(neighbor)==0){
+                     q.push(neighbor);
+                    visited.insert(neighbor);
+                }
+            }
+        }
+        return visited.size()==rooms.size();
+    }
+};
